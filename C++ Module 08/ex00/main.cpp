e457096b4cca4e7463b33d2727613b73e6ee83c1
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2023-01-05 14:04:37 +1030

    Update main.cpp

--- C++ Module 08/ex00/main.cpp
+++ C++ Module 08/ex00/main.cpp
@@ -18,15 +18,11 @@ int main()
 	std::vector<int>::iterator it;
 
 	for (int i = 0; i < 10; ++i) 
-	{
 		v.push_back(i * i);
-	}
 	std::cout << "Vector:" << std::endl;
 	
 	for (std::vector<int>::iterator i = v.begin(); i != v.end(); ++i) 
-	{
 		std::cout << *i << std::endl;
-	}
 	std::cout << std::endl;
 
 	it = easyfind(v, -3);
@@ -44,6 +40,5 @@ int main()
 	it = easyfind(v, 81);
 	if (it == v.end()) std::cout << "Not found" << std::endl;
 	else std::cout << "iterator: " << *it << std::endl;
-
 	return 0;
 }
