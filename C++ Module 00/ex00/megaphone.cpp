ff802dfdd59f3ce991a833db3f18c4832bc8b5d9
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2023-05-02 21:37:52 +0930

    Update megaphone.cpp

--- C++ Module 00/ex00/megaphone.cpp
+++ C++ Module 00/ex00/megaphone.cpp
@@ -15,16 +15,19 @@
 #include <locale>
 #include <stdio.h>
 
-int	main(int argc, char **argv)
+int    main(int argc, char **argv)
 {
-	if (argc == 1)
-		std::cout << "LOUD AND UNBEARABLE FEEDBACK NOISE" << std::endl;
-	else
-	{
-		for (int i = 1; i < argc; i++)
-			for (int j = 0; argv[i][j]; j++)
-				std::cout << (char)toupper(argv[i][j]);
-	}
-	std::cout << std::endl;
-	return (0);
+    if (argc == 1)
+        std::cout << "LOUD AND UNBEARABLE FEEDBACK NOISE" << std::endl;
+    else
+    {
+        for (int i = 1; i < argc; i++)
+        {
+            for (int j = 0; argv[i][j]; j++)
+                std::cout << (char)toupper(argv[i][j]);
+            (i != argc)? std::cout << " " : std::cout << "\n";
+        }
+    }
+    std::cout << std::endl;
+    return (0);
 }
